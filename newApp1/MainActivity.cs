d6eb18a23ead93e71581327d19b6284bd026e095
lucaspereira06 <lucas.gonsalves06@gmail.com>
2016-11-07 19:12:56 -0200

    Check-in inicial do módulo XamarinControle

---
+++ newApp1/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace newApp1
+{
+	[Activity(Label = "newApp1", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			ActionBar.Hide();
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
