5f1f75aeccd157bc65abad4cdcb28f73897dfa14
lucaspereira06 <lucas.gonsalves06@gmail.com>
2016-11-11 17:48:44 -0200

    Criação de toolbar

--- newApp1/MainActivity.cs
+++ newApp1/MainActivity.cs
@@ -1,20 +1,44 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.AppCompat;
+using Android.Support.Graphics.Drawable;
 
 namespace Controle.App
 {
-	[Activity(Label = "Controle", MainLauncher = true)]
-	public class MainActivity : Activity
+	[Activity(Label = "Controle", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+	public class MainActivity : AppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
-			ActionBar.Hide();
-
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+
+			var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);
+
+		}
+
+		public override bool OnCreateOptionsMenu(Android.Views.IMenu menu)
+		{
+			var inflater = MenuInflater;
+			inflater.Inflate(Resource.Menu.toolbar, menu);
+			return true;
+		}
+
+		public override bool OnOptionsItemSelected(Android.Views.IMenuItem item)
+		{
+			int id = item.ItemId;
+
+			switch (id)
+			{
+				case Resource.Id.teste: Toast.MakeText(this, "Teste clicado", ToastLength.Short).Show(); break;
+			}
+
+			return base.OnOptionsItemSelected(item);
 		}
 	}
 }
